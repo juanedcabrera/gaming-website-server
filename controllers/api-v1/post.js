a375af0e8f9eed2c8bfca1ad7bbbdfd729f5cc18
juanedcabrera <juanedcabrera@gmail.com>
2023-09-27 13:19:45 -0600

    Updatged post for PUT and DELETE

--- controllers/api-v1/post.js
+++ controllers/api-v1/post.js
@@ -63,6 +63,45 @@ router.post('/:gameId', authLockedRoute, async (req, res) => {
   }
 });
 
+// PUT /post/:postid - update a post
+router.put('/:postid', authLockedRoute, async (req, res) => {
+  try {
+    // find post by id and update
+    const updatePost = await Post.findByIdAndUpdate(
+      req.params.postid,
+      {
+        title: req.body.title,
+        content: req.body.content,
+        image: req.body.image,
+        videoLink: req.body.videoLink,
+      },
+      { new: true }
+    );
+    // send res with updatePost object
+    res.json({ updatePost });
+  } catch (error) {
+    // log error
+    console.log(error);
+    // return 500 error if something goes wrong
+    res.status(500).json({ msg: 'internal server error' });
+  }
+});
+
+// DELETE /post/:postid - delete a post
+router.delete('/:postid', authLockedRoute, async (req, res) => {
+  try {
+    // find post by id and delete
+    const deletePost = await Post.findByIdAndDelete(req.params.postid);
+    // send res with deletePost object
+    res.json({ deletePost });
+  } catch (error) {
+    // log error
+    console.log(error);
+    // return 500 error if something goes wrong
+    res.status(500).json({ msg: 'internal server error' });
+  }
+});
+
 // GET /:postid - get specific post by post id
 router.get('/:postid', async (req, res) => {
   try {
